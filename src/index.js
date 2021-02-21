f71b8dbcd722faa69139e01a4f09169b1398f6e4
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-02-21 15:26:15 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 // ----------------------------1----------------------
 // function calculateTotalPrice(orderedItems) {
 //   let totalPrice = 0;
@@ -15,7 +14,7 @@
 // ----------------------------2----------------------
 // function filterArray(numbers, value) {
 //   const filteredNumbers = [];
-  
+
 //   numbers.forEach(function(number){
 //     if (number > value){
 //     filteredNumbers.push(number);
@@ -23,15 +22,15 @@
 //   });
 
 //     return filteredNumbers;
-// }  
+// }
 // console.log(filterArray([1, 2, 3, 4, 5], 3)); //[4, 5]
 // console.log(filterArray([1, 2, 3, 4, 5], 4)); //[5]
 // console.log(filterArray([1, 2, 3, 4, 5], 5)) // []
 
 // ----------------------------3----------------------
 // function getCommonElements(firstArray, secondArray) {
-//   const commonElements = []; 
-  
+//   const commonElements = [];
+
 //   //   for (let i = 0; i < firstArray.length; i += 1) {
 //   //     if (secondArray.includes(firstArray[i])) {
 //   //       commonElements.push(firstArray[i]);
@@ -43,9 +42,69 @@
 //       commonElements.push(element)
 //     }
 //   });
-  
+
 //     return commonElements;
 //   }
 // console.log(getCommonElements([1, 2, 3], [2, 4])); // [2]
 // console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19])); // [1, 2]
-// ----------------------------4----------------------
+
+// ----------------------------4-5----------------------
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+// console.log(calculateTotalPrice(5, 100)); //500
+
+// ----------------------------6----------------------
+// const calculateTotalPrice = (orderedItems)=> {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
+
+//   return totalPrice;
+// }
+
+// ----------------------------7----------------------
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach((number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   return filteredNumbers;
+// };
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+
+// ----------------------------8----------------------
+// const getCommonElements = (firstArray, secondArray) => {
+//     const commonElements = [];
+
+//     firstArray.forEach((element) => {
+//       if (secondArray.includes(element)) {
+//         commonElements.push(element);
+//       }
+//     });
+
+//     return commonElements;
+// }
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])); // [12, 27, 3]
+
+// ----------------------------9----------------------
+// const changeEven = (numbers, value) => {
+//   const newArray = [];
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   if (numbers[i] % 2 === 0) {
+//   //     numbers[i] = numbers[i] + value;
+//   //   }
+//   // }
+//   numbers.forEach((number) => {
+//     number % 2 === 0 ? newArray.push(number + value) : newArray.push(number);
+//   });
+//   return newArray;
+// };
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10)); // [12, 18, 3, 7, 14, 16]
+
+// ----------------------------10----------------------
